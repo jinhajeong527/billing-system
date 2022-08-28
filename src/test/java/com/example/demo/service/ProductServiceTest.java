d000a8423fec0308121f91a3c047ad997de87894
jinha <jinha4ever@gmail.com>
2022-08-28 10:09:54 +0900

    ProductServiceTest 테스트 코드 수정

--- src/test/java/com/example/demo/service/ProductServiceTest.java
+++ src/test/java/com/example/demo/service/ProductServiceTest.java
@@ -6,20 +6,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.dto.PostAndPutProductPayload;
+import com.example.demo.entity.PriceHistory;
 import com.example.demo.entity.Product;
 import com.example.demo.exception.PriceInfoNotExistException;
 import com.example.demo.exception.ProductInfoNotExistException;
+import com.example.demo.exception.ProductNotFoundException;
 import com.example.demo.model.ProductTypeEnum;
 import com.example.demo.repository.PriceHistoryRepository;
 import com.example.demo.repository.ProductChangeHistoryRepository;
 import com.example.demo.repository.ProductRepository;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceTest {
     @Autowired
     ProductService productService;
@@ -32,7 +37,7 @@ public class ProductServiceTest {
     
     // 상품 정보 정상적으로 등록이 되어야 한다.
     @Test
-    void shouldCreateProduct() throws Exception {
+    void shouldCreateUpdateAndDeleteProduct() throws Exception {
         PostAndPutProductPayload postAndPutProductPayload = 
                         new PostAndPutProductPayload(ProductTypeEnum.APM, "Application", 0.1F, 
                                                      "CORE", new BigDecimal(25000.00));
@@ -44,7 +49,21 @@ public class ProductServiceTest {
         assertEquals(product.getName(), findProduct.getName());
         assertEquals(product.getMinCpu(), findProduct.getMinCpu());
         assertEquals(product.getChargeUnit(), findProduct.getChargeUnit());
-        assertEquals(product.getPriceHistories().get(0), findProduct.getPriceHistories().get(0));
+        assertEquals(product.getPriceHistories().get(0).getPrice(), findProduct.getPriceHistories().get(0).getPrice());
+
+        // Product 정보를 수정한다.
+        findProduct.setMinCpu(0.2F);
+        PriceHistory priceHistory = new PriceHistory(new BigDecimal(30000.00));
+        findProduct.add(priceHistory);
+        Product editedProduct = productRepository.save(findProduct);
+        assertEquals(editedProduct.getMinCpu(), 0.2F);
+        assertEquals(priceHistoryRepository.findFirstByProductOrderByCreateDateDesc(editedProduct).getPrice(), new BigDecimal(30000.00));
+
+        // 해당 상품을 지운다.
+        productRepository.delete(editedProduct);
+        ProductNotFoundException e = assertThrows(ProductNotFoundException.class,
+                    () -> productService.deleteProduct(editedProduct.getId())); //예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("No product found with this id: " + editedProduct.getId());
     }
 
 
@@ -59,7 +78,7 @@ public class ProductServiceTest {
                
         assertThat(e.getMessage()).isEqualTo("Product info is needed to register new product");
     }
-    
+
     // 상품 가격 정보가 등록되어 있지 않을 경우
     @Test
     void shouldReturnPriceInfoNotExistException() throws Exception {
